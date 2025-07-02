d9409025ab7331df2314a9e6d9d7dfbeb7d36e99
lsaperi <lsaperi>
2025-07-02 10:45:03 -0300

    bgfdfdsfds

--- TABLEs/Creation-Scripts/employees.sql
+++ TABLEs/Creation-Scripts/employees.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `employees` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
