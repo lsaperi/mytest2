dfda23e9bb0a08f63bcf9e02ac4a79d5d7d052db
lsaperi <lsaperi>
2025-07-02 10:57:41 -0300

    gfgdf

--- TABLEs/Creation-Scripts/employees.sql
+++ TABLEs/Creation-Scripts/employees.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `employees` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
+  `employee_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `department_id` int DEFAULT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `fk_department` (`department_id`),
+  CONSTRAINT `fk_department` FOREIGN KEY (`department_id`) REFERENCES `departments` (`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
--- TABLEs/Creation-Scripts/products.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
-  `price` decimal(10,2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
